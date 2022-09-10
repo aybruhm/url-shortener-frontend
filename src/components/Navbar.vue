38cfb817b1a9a0130736dd628b6ae0ee1d32abd6
Abram <israelvictory87@gmail.com>
2022-09-10 23:39:11 +0100

    Created navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar">
+        <a class="navbar-brand" href="#">
+            <img src="../assets/shortener-logo.png" alt="shortener-logo" class="img-responsive">
+        </a>
+    </nav>
+</template>
+
+
+<script>
+
+export default {
+    name: "Navbar"
+}
+</script>
+
+
+<style scoped>
+nav.navbar {
+    padding: 30px;
+    box-shadow: 0px 0px 0px 0px;
+}
+
+a.navbar-brand>img {
+    width: 250px;
+}
+
+@media screen and (max-width: 489px) {
+    a.navbar-brand>img {
+        width: 200px;
+    }
+}
+</style>
