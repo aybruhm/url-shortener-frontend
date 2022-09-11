0547632563e0298cba02fea59fe16b86e8053b1b
Abram <israelvictory87@gmail.com>
2022-09-11 13:21:06 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,21 @@ html,
 body {
   font-family: 'Poppins', sans-serif;
 }
+
+h4.main-subtitle {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  margin-top: 0%;
+  padding-top: 0%;
+  line-height: 40px;
+  letter-spacing: 0.065em;
+  color: #000000;
+}
+
+@media screen and (max-width: 489px) {
+  h4.main-subtitle {
+    font-size: 18px;
+  }
+}
 </style>
